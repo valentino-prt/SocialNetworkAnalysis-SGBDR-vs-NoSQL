f1f7702769a559ea06f30822dadc302de9f3437a
lucadeslot <deslot.luca@gmail.com>
2024-02-27 22:11:41 +0100

    Insertion is working

--- SocialNetworkAnalysis-NoSQL/src/main/java/org/example/socialnetworkanalysisnosql/repos/user/UserRepo.java
+++ SocialNetworkAnalysis-NoSQL/src/main/java/org/example/socialnetworkanalysisnosql/repos/user/UserRepo.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface UserRepo extends Neo4jRepository<User, Long>, CustomUserRepository {
 
-    @Query("MATCH (user:User) WITH user LIMIT 100000 DETACH DELETE user")
+    @Query("MATCH (user:User) CALL { WITH user DETACH DELETE user} IN TRANSACTIONS")
     void dumpLimit();
 
 }
